88df1086e3a7b26626c1554120889e2c6ea5a35c
dauid64 <carlosdauid64br@gmail.com>
2025-01-08 01:05:24 -0300

    adicionando configurações de deploy

--- facilitando_bits/settings.py
+++ facilitando_bits/settings.py
@@ -96,7 +96,8 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
